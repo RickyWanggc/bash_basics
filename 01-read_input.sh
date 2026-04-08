8fd2969dc568f6b4602e26a011a867116e347a60
Ricky Wang <rwang1@ggc.edu>
2026-04-08 19:06:42 -0400

    Edited bash script exercise

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # read the name of the user and print hello
 
-echo "Hello! What is your name"
-read name
-echo "Welcome, $name"
+echo "Hello! What is your first name?"
+read firstname
+echo "what is your last name?"
+read lastname
+echo "Welcome, $firstname $lastname"
 
 # single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
+echo 'Your name was stored in $firstname $lastname'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
